d1b38cf5ddb25e6779a79c29946c94ca7f65e336
wasif_alam <wsfalam84@gmail.com>
2024-04-04 18:23:04 +0530

    remove toml file, add proxy to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	nitro: {
+		routeRules: {
+			"/**": {
+				proxy: "//https://slotbooker.netlify.app/" + "/calendar/**",
+			},
+		},
+	},
 	devtools: { enabled: false },
 	modules: ["@nuxt/ui"],
 	colorMode: {
