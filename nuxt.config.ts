1a0e7698649f5d913f1e4f9a7611d04122d412a2
wasif_alam <wsfalam84@gmail.com>
2024-04-04 18:37:13 +0530

    modify proxy in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	routeRules: {
-		"/**": {
-			proxy: "https://slotbooker.netlify.app/calendar/**",
+		"/calendar/**": {
+			proxy: "https://slotbooker.netlify.app/**",
 		},
 	},
 
